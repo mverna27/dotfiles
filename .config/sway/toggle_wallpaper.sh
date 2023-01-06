a0696ec5b6c33fc570f23884b87cf23a03aaf23a
Marco Verna <mverna27@gmail.com>
2023-01-06 17:44:40 +0100

    wallpaper switcher

---
+++ .config/sway/toggle_wallpaper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+if ( readlink $HOME/.config/sway/active_wallpaper | grep private &>/dev/null )
+then
+	ln -sf $HOME/.config/sway/wallpaper_profiles/work $HOME/.config/sway/active_wallpaper
+elif ( readlink $HOME/.config/sway/active_wallpaper | grep work &>/dev/null )
+then
+	ln -sf $HOME/.config/sway/wallpaper_profiles/private $HOME/.config/sway/active_wallpaper
+fi
+
+swaymsg reload
