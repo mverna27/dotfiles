7e01f9b44842490f0f87b4341b35070b443fe5e4
Marco Verna <mverna27@gmail.com>
2023-01-09 13:16:10 +0100

    bluetooth control in waybar

--- Dropbox/Documents/linux/thinkpad/scripts/bluetooth_power_toggle_and_connect_airpods.sh
+++ Dropbox/Documents/linux/thinkpad/scripts/bluetooth_power_toggle_and_connect_airpods.sh
@@ -2,7 +2,14 @@
 if ( bluetoothctl show | grep "Powered: no" &>/dev/null )
 then
 	bluetoothctl power on &>/dev/null
-	bluetoothctl connect C4:35:D9:1C:9D:85 &>/dev/null
+	timeout 10 bluetoothctl connect C4:35:D9:1C:9D:85 &>/dev/null
+    if [ $? -ne 0 ]
+    then
+	    bluetoothctl power off &>/dev/null
+        exit 1
+    fi
+    exit 0
 else
 	bluetoothctl power off &>/dev/null
+    exit 0
 fi
